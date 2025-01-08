49876961087985e5b9ae7733a16ea41c17fb2bc1
Jhamyllie <milli.santana2017@gmail.com>
2025-01-08 15:47:21 -0300

    feat: Adiciona task para atualizar produtos

--- src/controller/produtos.js
+++ src/controller/produtos.js
@@ -34,7 +34,6 @@ const adicionarProduto = (req, res) => {
 };
 
 // listar produtos
-
 const listarProdutos = (req, res) => {
     try {
       const produtos = JSON.parse(fs.readFileSync(dataPath, "utf8"));
@@ -43,7 +42,63 @@ const listarProdutos = (req, res) => {
       res.status(500).json({ message: "Erro ao carregar os produtos", error });
     }
 }
+
 // atualizar produtos
+const atualizarProduto = (req, res) => {
+    try {
+        const { id } = req.params;
+        const{ NomeProduto, Categoria, QuantidadeEstoque, Preco } = req.body;
+
+        const produtos = JSON.parse(fs.readFileSync(dataPath, "utf8"));
+
+        const produtoIndex = produtos.findIndex((item) => item.ID === id);
+        if(produtoIndex < 0){
+            return res.status(404).json({message: "Produto não encontrado"});
+        }
+
+        const produto = produtos[produtoIndex];
+
+        if(NomeProduto){
+            if(NomeProduto.trim() === ""){
+                return res.status(400).json({message: "Nome do produto não pode está vazio"})
+            }
+            produto.NomeProduto = NomeProduto;
+        }
+
+        if(Categoria){
+            if(Categoria.trim() === ""){
+                return res.status(400).json({message: "Categoria não pode está vazia"})
+            }
+            produto.Categoria = Categoria;
+        }
+
+        if(QuantidadeEstoque){
+            const quantidade = parseInt(QuantidadeEstoque, 10)
+            if(isNaN(quantidade) || quantidade < 0){
+                return res.status(400).json({message: "Quantidade em estoque deve ser um número válido e maior ou igual a zero"});
+            }
+            produto.QuantidadeEstoque = quantidade.toString();
+        }
+
+        if (Preco) {
+            const precoFloat = parseFloat(Preco);
+            if (isNaN(precoFloat) || precoFloat <= 0) {
+              return res.status(400).json({ message: "Preço deve ser um número válido e maior que zero." });
+            }
+            produto.Preco = `R$ ${precoFloat.toFixed(2).replace(".", ",")}`;
+        }
+
+        produtos[produtoIndex] = produto;
+        fs.writeFileSync(dataPath, JSON.stringify(produtos, null, 2));
+
+        res.status(200).json({
+            message: "Produto atualizado com sucesso!",
+            produto,
+        });
+    } catch (error) {
+        
+    }
+}
 
 // excluir produtos
 
@@ -52,6 +107,7 @@ const listarProdutos = (req, res) => {
 
 module.exports = {
   adicionarProduto,
-  listarProdutos
+  listarProdutos,
+  atualizarProduto
 };
 
--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const router = express.Router();
-const { adicionarProduto, listarProdutos } = require("../controller/produtos");
+const { adicionarProduto, listarProdutos, atualizarProduto } = require("../controller/produtos");
 
 // Rota para manipular os produtos
 router.post("/adicionar", adicionarProduto);
-router.get("/", listarProdutos)
+router.get("/", listarProdutos);
+router.put("/:id", atualizarProduto);
 
 module.exports = router;
